89ebbef5003c69ced6213921a3643404dda20d68
crdiam <angiziewska@gmail.com>
2019-11-06 22:46:59 +0100

    Opening but not working file dialog

--- Filters.py
+++ Filters.py
@@ -1,16 +1,29 @@
 import sys
 from PyQt5.QtWidgets import *
 from FiltersUI import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+
 class MyForm(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_Filters()
         self.ui.setupUi(self)
-        self.ui.dilatationButton.clicked.connect(self.dispmessage)
+        self.ui.loadButton.clicked.connect(self.OpenFileNameDialog)
         self.show()
-    def dispmessage(self):
-        self.ui.filtersLabel.setText("Hello "
-        +self.ui.filtersLabel.text())
+
+    def OpenFileNameDialog(self):
+        fname = QFileDialog.getOpenFileName(self, 'Open file',
+                                            'c:\\', "Image files (*.jpg *.gif *.png)")
+
+        dlg = QFileDialog()
+        dlg.setFileMode(QFileDialog.FileType)
+        dlg.setFilter("Image files (*.jpg *.gif *.png)")
+      #  filenames = QStringList()
+
+      #  if dlg.exec_():
+       #     filenames = dlg.selectedFiles()
 def main():
     app = QApplication([])
     w = MyForm()
